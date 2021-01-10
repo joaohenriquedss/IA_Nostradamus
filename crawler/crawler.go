3140a372a6bec920d409db72739a47b162b431a5
Filipe <filipe.lima@ccc.ufcg.edu.br>
2021-01-10 20:43:50 -0300

    Change data to utf8 and remove numeric titles

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"github.com/saintfish/chardet"
+	"golang.org/x/net/html/charset"
+	"io/ioutil"
 	"os"
+	"regexp"
 )
+
+var (
+	removeNumTitle = regexp.MustCompile("(?m)^\\d+$")
+	detector = chardet.NewTextDetector()
+)
+
+func convertToUTF8(strBytes []byte, origEncoding string) ([]byte, error) {
+	byteReader := bytes.NewReader(strBytes)
+	reader, err := charset.NewReaderLabel(origEncoding, byteReader)
+	if err != nil {
+		return nil, err
+	}
+	strBytes, err = ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return strBytes, nil
+}
+
 func WriteFile(filename string, data string) error {
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
@@ -14,14 +39,28 @@ func WriteFile(filename string, data string) error {
 	if err1 := f.Close(); err == nil {
 		err = err1
 	}
+
 	return err
+}
+
+func getTextEncoding(dataBytes []byte) (string, error) {
+	result, err := detector.DetectBest(dataBytes)
+	if err != nil {
+		return "", err
+	}
+	return result.Charset, nil
 }
 
 func main() {
 	c := colly.NewCollector()
 
 	c.OnHTML("html", func(e *colly.HTMLElement) {
-		err := WriteFile("data/nos.txt", e.ChildText("p") + "\n")
+		text, err  := processText(e.ChildText("p") + "\n")
+		if err != nil {
+			panic(err)
+		}
+
+		err = WriteFile("data/nos.txt", text)
 		if err != nil {
 			panic(err)
 		}
@@ -37,4 +76,21 @@ func main() {
 	})
 
 	c.Visit("https://www.sacred-texts.com/nos/preface.htm")
+}
+
+func processText(text string) (string, error) {
+	dataBytes := []byte(text)
+	textEncoding, err := getTextEncoding(dataBytes)
+	if err != nil {
+		return "", err
+	}
+	if textEncoding != "UTF-8" {
+		dataBytes, err = convertToUTF8(dataBytes, textEncoding)
+		if err != nil {
+			return "", err
+		}
+	}
+	text = removeNumTitle.ReplaceAllString(string(dataBytes), "")
+
+	return text, nil
 }
